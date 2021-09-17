f4805fd7eae5b99bea0842ef40b94ccd04852429
Oskar Gierszewski <oskar@moodup.team>
2021-09-17 22:21:08 +0200

    Add side nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { AppBar, Toolbar, IconButton, Typography, Drawer } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 import React from 'react';
 import './App.css';
 import { Dashboard } from './pages/dashboard/dashboard';
 
 function App() {
-  return <Dashboard></Dashboard>;
+  const [state, setState] = React.useState({ drawer: true });
+  const toggleDrawer = (open: boolean) => {
+    console.log('toggle');
+    setState({ ...state, drawer: open });
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={() => setState({ ...state, drawer: true })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">Home</Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={state.drawer} onClose={() => setState({ ...state, drawer: false })}>
+        Side menu
+      </Drawer>
+      <Dashboard></Dashboard>
+    </>
+  );
 }
 
 export default App;
